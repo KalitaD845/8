f7fc2409db8c9185d390c766f755c0998914f007
KalitaD845 <65301478+KalitaD845@users.noreply.github.com>
2020-06-10 18:42:32 +0300

    Update 2_2.js

--- 2_2.js
+++ 2_2.js
@@ -1,1 +1,2 @@
-let name = prompt('введите ваше имя'); alert('Hello, ' + name + ', how are you doing today?')
+let name = prompt('введите ваше имя');
+alert('Hello, ' + name + ', how are you doing today?')
